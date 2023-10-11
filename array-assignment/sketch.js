4e45858ec1bd344ea181ca4e40d1ef7c0d0d9096
jacebaek1009 <jacebaek06@gmail.com>
2023-10-11 12:17:16 -0600

    started tag game

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -1,15 +1,32 @@
-// Project Title
+// Tag
 // Your Name
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let score = 0;
+let life = "alive";
+let recSide1 = 100;
+let recSdie2 = 100;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(220);
+}
+
+
+function displayPlayer(){
+  rect(x, y, recSide1, recSide2)
+}
+
+
+function movePlayer(){
+
 }
