413f793ebe53e363b9de26eb3eb973ae74591bf4
jacebaek1009 <jacebaek06@gmail.com>
2023-10-13 12:14:46 -0600

    making basic movement trying to add gravity

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -7,27 +7,32 @@
 
 let score = 0;
 let life = "alive";
-let recSide1 = 100;
-let recSide2 = 100;
+let recSide1 = 25;
+let recSide2 = 25;
+let x;
+let y;
+let vx;
+let vy;
+let grav = 0.2;
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  x = windowWidth/2;
+  y = windowHeight;
 }
 
 function draw() {
   background(220);
-  
+  displayPlayer();
 }
 
 
 function displayPlayer(){
-  rect(x, y, recSide1, recSide2)
+  fill(0);
+  rect(x, y - recSide2, recSide1, recSide2);
 }
 
+function keyIsDown{}
 
-function movePlayer(){
-
-}
