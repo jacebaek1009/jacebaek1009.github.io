4f65d09834033277b5798568f45cb2c94e0ffb9f
jacebaek1009 <jacebaek06@gmail.com>
2023-11-22 12:17:00 -0600

    added images

--- major-project/sketch.js
+++ major-project/sketch.js
@@ -6,11 +6,21 @@
 // - describe what you did to take this project "above and beyond"
 
 let ingredient = [];
+let salmon;
+let basket;
+let grab = true;
+let backg;
+
+function preload() {
+  salmon = loadImage("salmon.png");
+  basket = loadImage("basket.png");
+  backg = loadImage("")
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("white");
-  let place = new Ingredients(mouseX, mouseY, 10);
+  let place = new Ingredients(mouseX, mouseY, salmon);
   ingredient.push(place);
 }
 
@@ -18,23 +28,28 @@ function draw() {
   for(let i of ingredient) {
     i.display();
   }
+  displayBasket(windowWidth/2, windowHeight/2);
 }
 function mousePressed() {
-  let place = new Ingredients(mouseX, mouseY, 10);
+  let place = new Ingredients(mouseX, mouseY, salmon);
   ingredient.push(place);
 }
 
 
 class Ingredients {
-  constructor(x, y, size){
+  constructor(x, y, type){
     this.x = x;
     this.y = y;
-    this.size = size;
+    this.type = type;
   }
 
   display() {
     noStroke();
     fill(255, 0 , 0);
-    circle(this.x, this.y, this.size);
+    image(this.type, this.x, this.y);
   }
+}
+
+function displayBasket(x, y) {
+  image(basket, x, y);
 }
